97a16e9f36a28a77c2a0e230bdc85b8cf0aa4d4a
Jerry Jiang <jerryjiang1128@gmail.com>
2015-04-07 03:08:16 -0700

    Update software.sh

--- ubuntu/software.sh
+++ ubuntu/software.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
+# Mutate
+sudo add-apt-repository ppa:mutate/ppa
+sudo apt-get update
+sudo apt-get install mutate
+
+# Preload
 sudo apt-get install -y preload
+
+# Z-shell
 sudo apt-get install -y zsh
