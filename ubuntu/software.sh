a0ab524f63911d674b7c6bfe3191417622611c17
Jerry Jiang <jerryjiang1128@gmail.com>
2015-04-07 03:55:03 -0700

    Update software.sh

--- ubuntu/software.sh
+++ ubuntu/software.sh
@@ -11,3 +11,8 @@ sudo apt-get install -y preload
 # Z-shell + oh-my-zsh
 sudo apt-get install -y zsh
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+
+# Terminator and solarized colors for Terminator
+sudo apt-get install -y terminator
+curl https://raw.githubusercontent.com/ghuntley/terminator-solarized/master/config > temp.txt
+mv temp.txt ~/.config/terminator/config
