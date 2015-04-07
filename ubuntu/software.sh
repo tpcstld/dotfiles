3e803ed78e7396635804fb9eb8b311695e25d303
Jerry Jiang <jerryjiang1128@gmail.com>
2015-04-07 03:08:52 -0700

    Update software.sh

--- ubuntu/software.sh
+++ ubuntu/software.sh
@@ -8,5 +8,6 @@ sudo apt-get install mutate
 # Preload
 sudo apt-get install -y preload
 
-# Z-shell
+# Z-shell + oh-my-zsh
 sudo apt-get install -y zsh
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
