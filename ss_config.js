aa5d0736f72022d31ba71a2ebe9d565aa9a2a9dc
Jerry Jiang <jerryjiang1128@gmail.com>
2016-01-06 18:58:49 -0500

    Set font-size to 16

--- ss_config.js
+++ ss_config.js
@@ -7,3 +7,4 @@ term_.prefs_.set('background-color', '#002b36');
 term_.prefs_.set('cursor-color', '#eee8d5');
 term_.prefs_.set('foreground-color', '#eee8d5');
 term_.prefs_.set('font-family', '"ubuntu mono", monospace ');
+term_.prefs_.set('font-size', 16);
