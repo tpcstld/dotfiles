d96a124f7abc91a4eaaa5acce3a8b14a33a77c98
Jerry Jiang <jerry@tpcstld.me>
2016-01-06 15:39:49 +0000

    Add Secure Shell/hterm config file.

---
+++ ss_config.js
@@ -0,0 +1,8 @@
+// This contains the configuration file for Chrome's Secure Shell plugin.
+// Paste this into the javascript console while hterm/Secure Shell is running.
+
+// Solarized color scheme
+term_.prefs_.set('color-palette-overrides', {"0":"#073642","1":"#dc322f","2":"#859900","3":"#b58900","4":"#268bd2","5":"#d33682","6":"#2aa198","7":"#eee8d5","8":"#002b36","9":"#cb4b16","10":"#586e75","11":"#657b83","12":"#839496","13":"#6c71c4","14":"#93a1a1","15":"#fdf6e3"});
+term_.prefs_.set('background-color', '#002b36');
+term_.prefs_.set('cursor-color', '#eee8d5');
+term_.prefs_.set('foreground-color', '#eee8d5');
