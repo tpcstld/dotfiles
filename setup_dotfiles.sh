92cae36197e336e5d745e6610da7c8d1b562a06f
Jerry Jiang <jerry@tpcstld.me>
2016-04-11 16:48:40 -0400

    Add OSX specific tmux config.

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -10,10 +10,12 @@ ln -s $(pwd)/bash_profile.ubuntu ~/.bash_profile.ubuntu
 ln -s $(pwd)/gitconfig ~/.gitconfig
 ln -s $(pwd)/gitignore ~/.gitignore
 ln -s $(pwd)/tmux.conf ~/.tmux.conf
+ln -s $(pwd)/tmux.conf.osx ~/.tmux.conf.osx
 ln -s $(pwd)/vimrc.bundles.local ~/.vimrc.bundles.local
 ln -s $(pwd)/vimrc.local ~/.vimrc.local
 ln -s $(pwd)/vimrc.osx ~/.vimrc.osx
 ln -s $(pwd)/vimrc.ubuntu ~/.vimrc.ubuntu
 
 echo "source ~/.vimrc.osx" > ~/.vimrc.which
+echo "source-file ~/.tmux.conf.osx" > ~/.tmux.conf.which
 echo "source ~/.bash_profile.osx" > ~/.bash_profile.which
