aaa8c9a37c357c4bf197d56ad3cc56510855ad33
Jerry Jiang <jerry@tpcstld.me>
2016-04-11 16:17:35 -0400

    Delete save_dotfiles.

--- save_dotfiles.sh
+++
@@ -1,6 +0,0 @@
-cp ~/.vimrc vimrc
-cp ~/.vimrc.local vimrc.local
-cp ~/.vimrc.bundles vimrc.bundles
-cp ~/.vimrc.bundles.local vimrc.bundles.local
-cp ~/.bash_profile bash_profile
-cp ~/.tmux.conf tmux.conf
