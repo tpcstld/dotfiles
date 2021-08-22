10ab57549f80a7492015ea3f7c99cd53713dc35e
Jerry Jiang <jerry.jiang@discordapp.com>
2021-08-22 07:04:09 +0000

    Install ripgrep as well.

--- install.ubuntu.sh
+++ install.ubuntu.sh
@@ -2,3 +2,7 @@
 sudo add-apt-repository ppa:neovim-ppa/unstable --yes
 sudo apt-get update
 sudo apt-get install neovim
+
+sudo add-apt-repository ppa:x4121/ripgrep --yes
+sudo apt-get update
+sudo apt-get install ripgrep
