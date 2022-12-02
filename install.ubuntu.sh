6bff1e0a5d680984f7c7e50152daaf933b07559a
Jerry Jiang <jerry.jiang@discordapp.com>
2022-12-02 20:15:50 +0000

    asdf

--- install.ubuntu.sh
+++ install.ubuntu.sh
@@ -1,8 +1,15 @@
+sudo apt-get update
+
+sudo apt-get install silversearcher-ag --yes
+sudo apt-get install autojump --yes
+sudo apt-get install zsh --yes
 
 sudo add-apt-repository ppa:neovim-ppa/unstable --yes
 sudo apt-get update
-sudo apt-get install neovim
+sudo apt-get install neovim --yes
 
 sudo add-apt-repository ppa:x4121/ripgrep --yes
 sudo apt-get update
-sudo apt-get install ripgrep
+sudo apt-get install ripgrep --yes
+
+sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin --yes
