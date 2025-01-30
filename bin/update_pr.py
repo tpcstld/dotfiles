4f8c02d5a793f01171264c988db86f71b4fc2d5d
Jerry Jiang <jerry.jiang@discordapp.com>
2025-01-30 15:52:55 -0800

    update pr script.

--- bin/update_pr.py
+++ bin/update_pr.py
@@ -2,8 +2,7 @@
 import subprocess
 
 def find_current_commit(output):
-    lines = [o.strip('o|╷ ') for o in output]
-    lines = [o for o in lines if o.startswith('@')]
+    lines = [o for o in output if o.startswith('@')]
 
     assert len(lines) == 1
 
@@ -13,13 +12,14 @@ def find_current_commit(output):
     return line[1]
 
 def find_rewritten_commits(output):
-    lines = [o for o in output if 'Rewritten into' in o]
+    lines = [o for o in output if o.startswith('x')]
 
     result = {}
 
     for line in lines:
-        line = line[line.index('Rewritten into') + len('Rewritten into') + 1:]
-        new_commit = line[:line.index(']')]
+        line = line[line.index('['):]
+
+        new_commit = line[:line.index(']')].split()[-1]
         branch_name = line.split()[-1].removeprefix('origin/')
 
         result[new_commit] = branch_name
@@ -35,11 +35,14 @@ def main():
     output = subprocess.check_output(['sl', 'ssl']).strip()
     output = output.decode('utf-8')
     output = output.split('\n')
+    output = [o.strip('o|│ ╷ ') for o in output]
 
     current_commit = find_current_commit(output)
     rewritten_commits = find_rewritten_commits(output)
 
     branch = find_matching_branch(current_commit, rewritten_commits)
+
+    assert branch is not None
 
     print(f'sl push -t {branch} --force')
 
