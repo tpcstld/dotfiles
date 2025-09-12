bfe13e9de11ff7b06b30f84a0f1398a7235d9203
Jerry Jiang <jerry.jiang@discordapp.com>
2025-09-11 23:56:39 -0700

    More stuff

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>t", "<cmd>FzfLua git_files<cr>", { desc = "FzfLua git_files" })
+vim.keymap.set("n", "<leader>r", function()
+  Snacks.explorer()
+end, { desc = "File Tree (cwd)" })
+vim.keymap.set("n", "<leader>d", function()
+  Snacks.explorer({ cwd = LazyVim.root() })
+end, { desc = "File Tree (cwd)" })
