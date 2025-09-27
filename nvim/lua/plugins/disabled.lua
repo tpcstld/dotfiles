d1abd763eeedab44478ec385f9d106902dce69f9
Jerry Jiang <jerry.jiang@discordapp.com>
2025-09-27 00:50:49 +0000

    trouble.

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -4,4 +4,5 @@ return {
   { "hrsh7th/nvim-cmp", enabled = false },
   { "Saghen/blink.cmp", enabled = false },
   { "neovim/nvim-lspconfig", enabled = false },
+  { "folke/trouble.nvim", enabled = false },
 }
